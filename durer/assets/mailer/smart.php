06b16edf3fb0bde403f334b90dcc63c218ba9b67
Smirnov Nikolay <mrrblaack@gmail.com>
2020-04-07 03:51:18 +1000

    last-version

---
+++ durer/assets/mailer/smart.php
@@ -0,0 +1,45 @@
+<?php 
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$comment = $_POST['comment'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'durersendemail@gmail.com';                 // Наш логин
+$mail->Password = 'I7S23WEPwCBY';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('durersendemail@gmail.com', 'landing Durer');   // От кого письмо 
+$mail->addAddress('mrrblaack@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Это тема сообщения'; //тема сообщения
+$mail->Body    = '
+	Пользователь оставил свои данные <br> 
+	Имя: ' . $name . ' <br>
+	Телефон: ' . $phone . ' <br>
+	Сообщение: ' . $comment . '';
+$mail->AltBody = 'Это альтернативный текст';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
